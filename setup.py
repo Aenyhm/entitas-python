870b2a0931e57da0007d30c1a7691f01b0c3c65d
Fabien Nouaillat <aenyhm@gmail.com>
2017-01-30 23:31:11 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+packages = ['entitas']
+requires = ['pytest']
+
+with open('entitas/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='Entitas',
+    version=version,
+    description='Entitas ECS implementation in Python',
+    long_description=readme,
+    author='Fabien Nouaillat',
+    author_email='aenyhm@gmail.com',
+    url='https://github.com/aenyhm/entitas-python',
+    packages=packages,
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT'
+)
